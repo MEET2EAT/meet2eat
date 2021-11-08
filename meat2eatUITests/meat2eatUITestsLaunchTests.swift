847a42d84521f8b23edaef405a62920d469c031a
Luu <loluu@lib310382.ttu.edu>
2021-11-08 17:38:56 -0600

    simple prototype

---
+++ meat2eatUITests/meat2eatUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  meat2eatUITestsLaunchTests.swift
+//  meat2eatUITests
+//
+//  Created by Luu, Loc on 11/8/21.
+//
+
+import XCTest
+
+class meat2eatUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
