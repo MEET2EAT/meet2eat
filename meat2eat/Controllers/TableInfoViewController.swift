80da9c6efd936b24600b1c30c7947568797aa5c6
anlqt89 <82059787+anlqt89@users.noreply.github.com>
2021-11-21 12:59:53 -0600

    Create TableInfoViewController.swift

---
+++ meat2eat/Controllers/TableInfoViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TableInfoViewController.swift
+//  meat2eat
+//
+//  Created by Lam, An Q on 11/21/21.
+//
+
+import UIKit
+
+class TableInfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
