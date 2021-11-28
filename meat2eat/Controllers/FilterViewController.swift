dd898c3c4a7af131f0618a29d450e661d4a4c084
vincent5779 <vncent5779@gmail.com>
2021-11-28 17:15:51 -0600

    new filterViewController

---
+++ meat2eat/Controllers/FilterViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FilterViewController.swift
+//  meat2eat
+//
+//  Created by Hew, Vincent on 11/28/21.
+//
+
+import UIKit
+
+class FilterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
