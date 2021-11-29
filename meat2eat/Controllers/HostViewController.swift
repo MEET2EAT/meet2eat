a46e6b600b636ae4a46a7a1e85cdcec67ac5f40c
kuku20 <35443377+kuku20@users.noreply.github.com>
2021-11-29 10:13:18 -0600

    Update HostViewController.swift

--- meat2eat/Controllers/HostViewController.swift
+++ meat2eat/Controllers/HostViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import AlamofireImage
+import Parse
 class HostViewController: UIViewController {
     // Initialize restaurant variable
     @IBOutlet weak var nameLabel: UILabel!
@@ -19,6 +20,7 @@ class HostViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         configureOutlets()
+        //RestaurantsViewController .dismiss(animated:YES completion:nil)
       //  RestaurantsViewController.dismiss(animated: true, completion: nil)
 
         // Do any additional setup after loading the view.
@@ -30,26 +32,25 @@ class HostViewController: UIViewController {
             let x = addressFul[0] as! String
             let y = addressFul[1] as! String
             location.text = "Location:  " + x + y
+            print(r.imageURL)
 
         }
     }
 
 
     @IBAction func OnSetTableButton(_ sender: Any) {
-        /*
+
        let Table2Meet = PFObject(className: "Table2Meet")
        if(r != nil){ 
             Table2Meet["ResName"] = r.name
-            Table2Meet["ResImage"] = r.imageURL!
-            ///.af.setImage(withURL: r.imageURL!)
             var addressFul = r.location["display_address"]! as! NSArray
             let x = addressFul[0] as! String
             let y = addressFul[1] as! String
             Table2Meet["location"] = x + y
         }
         Table2Meet["current"] = 0
-        Table2Meet["slots"] = slots.text!
-        Table2Meet["dateMeet"] = dateMeet.text!
+        Table2Meet["slots"] = Int(slots.text!)
+        Table2Meet["dateMeet"] = dateMeet.date
         Table2Meet["detailMeet"] = detailMeet.text!
         Table2Meet["host"] = PFUser.current()!
         Table2Meet.saveInBackground { (succeeded, error)  in
@@ -61,9 +62,11 @@ class HostViewController: UIViewController {
                 // There was a problem, check error.description
                 print("error!")
         }  
-        */
+           
     }
-    
+        self.dismiss(animated: true, completion: nil)
+        HostViewController?.dismiss(animated: true, completion: nil)
+    }
     @IBAction func onBackButton(_ sender: Any) {
         // The object has been saved.
         self.dismiss(animated: true, completion: nil)
@@ -79,3 +82,4 @@ class HostViewController: UIViewController {
     */
 
 }
+
