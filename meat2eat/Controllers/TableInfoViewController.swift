9935f481d88baadccfb9dccc705e0b3c679563c9
anlqt89 <82059787+anlqt89@users.noreply.github.com>
2021-12-02 17:59:37 -0600

    GUI tableinfo

--- meat2eat/Controllers/TableInfoViewController.swift
+++ meat2eat/Controllers/TableInfoViewController.swift
@@ -6,15 +6,56 @@
 //
 
 import UIKit
+import Parse
 
-class TableInfoViewController: UIViewController {
+class TableInfoViewController: UIViewController{
+    
+    
+    
 
+    @IBOutlet weak var TableStatus: UILabel!
+    @IBOutlet weak var TableApproved: UILabel!
+    
+    @IBOutlet weak var RestaurantName: UILabel!
+    @IBOutlet weak var RestaurantLocation: UILabel!
+    
+    @IBOutlet weak var FilledSlots: UILabel!
+    
+
+    @IBOutlet weak var slotsCollectionView: UICollectionView!
+    
+    @IBOutlet weak var TotalSlots: UILabel!
+    let totalSlots_ = 9
+    
+    /// <#Description#>
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        slotsCollectionView.delegate = self
+        slotsCollectionView.dataSource = self
+        //let flowLayout = UICollectionViewFlowLayout()
+        //Create empty view cells
+        
+        
+        /*self.collectionView = UICollectionView(frame: self.view.bounds, collectionViewLayout: flowLayout)
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "SlotCell")
+        collectionView.layer.delegate = self
+        collectionView.dataSource = self
+                collectionView.backgroundColor = UIColor.cyan
+        self.view.addSubview(collectionView)
+        
+        // Do any additional setup after loading the view.*/
     }
     
+    func loadInfoTable(){
+        let Table2Meet = PFObject(className:"Table2Meet")
+        
+        let user = PFUser.current()
+        
+        if(user){
+            print(user.getstring())
+        }
+       
+    }
 
     /*
     // MARK: - Navigation
@@ -26,4 +67,33 @@ class TableInfoViewController: UIViewController {
     }
     */
 
+    
+    
+    
+}
+
+extension TableInfoViewController: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UICollectionViewDelegate, CALayerDelegate{
+         func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+            return totalSlots_;
+        }
+        
+     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "SlotCell", for: indexPath) as! SlotCollectionViewCell
+            
+            //cell.UserImage.af_setImage(withURL: <#T##URL#>)
+            cell.UserName.text = "Ikemen Kuma"
+            //cell.backgroundColor = UIColor.green
+            return cell
+        }
+    
+  //  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: //UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize
+   // {
+   //     return CGSize(width: 50, height: 50)
+    //}
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1;
+    }
+    
+    
 }
