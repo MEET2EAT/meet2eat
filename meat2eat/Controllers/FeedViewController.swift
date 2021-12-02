cb23a374d6e843231cf65991c58def0cd7ddb457
bkhanhho <buukhanh.ueh@gmail.com>
2021-12-01 22:16:39 -0600

    Fixed Logout button bug

--- meat2eat/Controllers/FeedViewController.swift
+++ meat2eat/Controllers/FeedViewController.swift
@@ -150,20 +150,9 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
 
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
     
     @IBAction func onLogoutButton(_ sender: Any) {
         PFUser.logOut()
-        
         let main = UIStoryboard(name: "Main", bundle: nil)
         let loginViewController = main.instantiateViewController(withIdentifier: "LoginViewController")
         guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene, let delegate = windowScene.delegate as? SceneDelegate else { return }
