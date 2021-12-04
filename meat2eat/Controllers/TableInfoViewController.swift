b5d5cab078079065bdaec8aa573690ab1a4150e6
anlqt89 <82059787+anlqt89@users.noreply.github.com>
2021-12-03 19:50:27 -0600

    see Table info

--- meat2eat/Controllers/TableInfoViewController.swift
+++ meat2eat/Controllers/TableInfoViewController.swift
@@ -25,7 +25,7 @@ class TableInfoViewController: UIViewController, UICollectionViewDataSource, UIC
     @IBOutlet weak var slotsCollectionView: UICollectionView!
     
     @IBOutlet weak var TotalSlots: UILabel!
-    let totalSlots_ = 9
+    var totalSlots_ = 12
     var filledSlots = 0;
     let tableId = "1zBlQfNccR"
     var table2Meet = [PFObject]()
@@ -77,14 +77,6 @@ class TableInfoViewController: UIViewController, UICollectionViewDataSource, UIC
         super.viewDidAppear(animated)
         loadTable2Eat()
         
-        let query = PFQuery(className: "user")
-       // query.includeKey("users"
-        query.limit = 1
-     
-        query.findObjectsInBackground{(Table, error) in
-                print("GET USER")
-                print(Table)
-            }
     }
     
      func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -96,75 +88,42 @@ class TableInfoViewController: UIViewController, UICollectionViewDataSource, UIC
             if(self.filledSlots > indexPath.row){
                 print((self.filledSlots > indexPath.row))
 
-//                 let query = PFQuery(className: "User")
-//                 query.includeKeys(["username", "image"])
-//                 print("SEEIDDD")
-//                 print(self.userIdList[indexPath.row])
-//                 //query.whereKey("objectId", contains: self.userIdList[indexPath.row])
-//                 query.limit = 1
              
-                var query = PFUser.query()
-        query!.whereKey("username", equalTo:"loc")
-        
-        do {
-            let users: [PFObject] = try query?.findObjects() as! [PFObject]
-            print(users)
-        } catch {
-            print(error)
-        }
-                
-                
-                
-                query.findObjectsInBackground{(user_, error) in
-                    //print("DDDDCELLLLLLLLLLLL")
-                    //print(user_)
-                       // var userOj =  [PFObject]()
-                       // userOj = user_!
-                      //  let userTuple = userOj[0]
-                                          
-                      //  cell.UserName.text = userTuple["username"] as? String
-                        
-                      //  let imageFile = userTuple["image"] as!PFFileObject
-                      //  let urlString = imageFile.url!
-                      //  let url = URL(string: urlString)!
-                      //  cell.UserImage.af.setImage(withURL: url)
-                        
+                let query = PFUser.query()
+                query!.whereKey("objectId", equalTo: self.userIdList[indexPath.row])
+                print(self.userIdList[indexPath.row])
+                do {
+                    let user_ = try query?.findObjects() as! [PFObject]
+
+                    print("DDDDCELLLLLLLLLLLL")
+                    print(user_)
+                    if(user_.isEmpty == false){
+                        let userTuple = user_[0]
+
+                        cell.UserName.text = userTuple["username"] as? String
+
+                        let imageFile = userTuple["image"] as!PFFileObject
+                        let urlString = imageFile.url!
+                        let url = URL(string: urlString)!
+                        cell.UserImage.af.setImage(withURL: url)
                     }
+                } catch {
+                    print(error)
+                }
                 
-                   //cell.UserImage.af_setImage(withURL: <#T##URL#>)
-                    //cell.backgroundColor = UIColor.green
-             }
+                
+                    
+                
+            }
+
         
             return cell
         }
-    
-  //  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: //UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize
-   // {
-   //     return CGSize(width: 50, height: 50)
-    //}
     
     func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 1;
     }
     
-    func loadGuest(userId_: String) -> Array<String>{
-        var userInfo = [String]()
-        let query = PFQuery(className: "User")
-        query.includeKey("users")
-        query.whereKey("objectId", contains: userId_)
-        query.limit = 1
-     
-        query.findObjectsInBackground{(user_, error) in
-                let userOj = user_!
-                let userTuple = userOj[0]
-                userInfo[0] = userTuple["username"] as! String
-                userInfo[1] = userTuple["image"] as! String
-                self.filledSlots = self.userIdList.count
-                print(self.filledSlots)
-                self.slotsCollectionView.reloadData()
-            }
-        return userInfo
-    }
     func loadTable2Eat(){
         
        
@@ -178,11 +137,20 @@ class TableInfoViewController: UIViewController, UICollectionViewDataSource, UIC
                 self.table2Meet = Table!
             
                 let table = self.table2Meet[0]
-                               
-                self.userIdList = (table["guestsId"] as! [String]) ?? []
+                print("ttabkbk")
+                print(table)
+            self.userIdList = (table["guestsId"] as! [String])
                 self.filledSlots = self.userIdList.count
+            self.totalSlots_ = (table["slots"] as! Int)
                 print(self.filledSlots)
-                self.slotsCollectionView.reloadData()
+                
+            self.TotalSlots.text = "\(self.totalSlots_)"
+            self.FilledSlots.text = "\(self.userIdList.count)"
+                self.RestaurantName.text = "\(table["ResName"] as! String)"
+            self.RestaurantLocation.text = "\(table["location"] as! String)"
+            self.TableApproved.text = "\(table["detailMeet"] as! String)"
+        
+            self.slotsCollectionView.reloadData()
             }
         
        
